258355e2ac54f5bd2ad44e73f84efaa0ac1a82b2
Jean-Baptiste Lièvremont <jeanbaptiste.lievremont@sonarsource.com>
2018-08-07 11:16:47 +0200

    Update to new URL scheme, add branch/PR links

--- userscripts/add-burgr-button-to-github.js
+++ userscripts/add-burgr-button-to-github.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github Burgr Button
 // @namespace    https://burgr.sonarsource.com/
-// @version      0.1
+// @version      0.2
 // @description  Add a "Show in Burgr" button to SonarSource Github repositories
 // @author       jeanbaptiste.lievremont@sonarsource.com
 // @match        https://github.com/SonarSource/*
@@ -15,13 +15,26 @@
 (function() {
   'use strict';
 
-  var headerNode = document.querySelector('.repohead-details-container h1');
-  var projectName = /https:\/\/github\.com\/SonarSource\/([^/]+)/.exec(window.location.href)[1];
-  var burgrLink = document.createElement('a');
-  burgrLink.href = "https://burgr.sonarsource.com/project/SonarSource/" + projectName;
-  burgrLink.target = "_blank";
-  burgrLink.rel = "noopener noreferer";
-  burgrLink.title = "Show in Burgr";
-  burgrLink.innerHTML = "🍔";
+  const projectExp = /https:\/\/github\.com\/SonarSource\/([^/]+)/;
+  const branchExp = /\/tree\/([\w\/]+)/;
+  const prExp = /\/pull\/(\d+)/;
+
+  const location = window.location.href;
+  const projectName = projectExp.exec(location)[1];
+  const branchName = branchExp.test(location) ? branchExp.exec(location)[1] : null;
+  const prNumber = prExp.test(location) ? prExp.exec(location)[1] : null;
+
+  const headerNode = document.querySelector('.repohead-details-container h1');
+  const burgrLink = document.createElement('a');
+  burgrLink.href = 'https://burgr.sonarsource.com/projects/SonarSource/' + projectName;
+  if (branchName) {
+    burgrLink.href += ('/branches/' + branchName);
+  } else if (prNumber) {
+    burgrLink.href += ('/pullrequests/' + prNumber);
+  }
+  burgrLink.target = '_blank';
+  burgrLink.rel = 'noopener noreferer';
+  burgrLink.title = 'Show in Burgr';
+  burgrLink.innerHTML = '🍔';
   headerNode.insertBefore(burgrLink, headerNode.childNodes[1]);
 })();
